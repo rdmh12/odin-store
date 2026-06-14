6bc3e765d3ab96d1e128110f1ee8edfda144f710
rdmh12 <276881386+rdmh12@users.noreply.github.com>
2026-06-14 10:32:23 +0200

    Extract SVG icons into separate files

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,6 +2,9 @@ import styles from "./Product.module.css";
 import starFull from "./img/star.svg";
 import starHalf from "./img/star-half.svg";
 import starEmpty from "./img/star-empty.svg";
+import trash from "./img/trash.svg";
+import minus from "./img/minus.svg";
+import plus from "./img/plus.svg";
 
 export default function Product({
   product,
@@ -32,11 +35,17 @@ export default function Product({
       <img src={product.image} alt={product.title} />
       <div className="stretcher"></div>
       <div className={styles.rating}>
-        <span aria-hidden="true" data-testid="product-rating">{product.rating.rate}</span>
+        <span aria-hidden="true" data-testid="product-rating">
+          {product.rating.rate}
+        </span>
         <span aria-label={`${product.rating.rate} of out 5 stars, rating`}>
           {stars}
         </span>
-        <span className={styles.ratingCount} aria-hidden="true" data-testid="product-rating-count">
+        <span
+          className={styles.ratingCount}
+          aria-hidden="true"
+          data-testid="product-rating-count"
+        >
           ({product.rating.count})
         </span>
         <span className="sr-only">{`${product.rating.count} ratings`}</span>
@@ -51,27 +60,10 @@ export default function Product({
             aria-label="Decrement"
             className={`${styles.amountButton} ${styles.amountButtonDecrement}`}
           >
-            {amount == 1 ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#555"
-              >
-                <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#555"
-              >
-                <path d="M200-440v-80h560v80H200Z" />
-              </svg>
-            )}
+            <img
+              src={amount == 1 ? trash : minus}
+              className={styles.buttonIcon}
+            />
           </button>
           <div className={styles.amount}>{amount}</div>
           <button
@@ -80,15 +72,7 @@ export default function Product({
             aria-label="Increment"
             className={`${styles.amountButton} ${styles.amountButtonIncrement}`}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#555"
-            >
-              <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
-            </svg>
+            <img src={plus} className={styles.buttonIcon} />
           </button>
         </div>
       ) : (
